f7e1cdb3c37b2f13d655bb70da8388bac6ed7d28
XCACTUS\maciej.wojtasik <maciej.wojtasik@xcactus.pl>
2017-03-21 12:34:07 +0100

    refactor, clean contacts, delete invisible content

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,112 +24,34 @@ app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 });
 
-app.controller('ImagesCtrl', function($scope, $cordovaImagePicker, $ionicPlatform, $cordovaContacts) {
+app.controller('ImagesCtrl', function($scope, $cordovaImagePicker, $ionicPlatform) {
   $scope.collection = {
-        selectedImage : ''
+    selectedImage : ''
+  };
+
+  $ionicPlatform.ready(function() {
+    $scope.getImage = function() {
+        var options = {
+            maximumImagesCount: 1,
+            width: 640,
+            height: 480,
+            quality: 100
+        };
+
+        // $cordovaImagePicker.getPictures(options)
+        $cordovaImagePicker.getPictures(options)
+          .then(function (results) {
+
+            for (var i = 0; i < results.length; i++) {
+              console.log('Image URI: ' + results[i]);
+              $scope.collection.selectedImage = results[i];
+            }
+        }, function(error) {
+            console.log('Error: ' + JSON.stringify(error));    // In case of error
+        });
     };
-  // this.$cordovaImagePicker = $cordovaImagePicker;
 
-    $ionicPlatform.ready(function() {
-
-          $scope.getImageSaveContact = function() {
-              var options = {
-                  maximumImagesCount: 1,
-                  width: 50,
-                  height: 50,
-                  quality: 100
-              };
-
-              // $cordovaImagePicker.getPictures(options)
-              $cordovaImagePicker.getPictures(options)
-                .then(function (results) {
-
-                  for (var i = 0; i < results.length; i++) {
-                    console.log('Image URI: ' + results[i]);
-                    $scope.collection.selectedImage = results[i];
-
-                  /*
-                    window.plugins.Base64.encodeFile($scope.collection.selectedImage, function(base64){  // Encode URI to Base64 needed for contacts plugin
-                          $scope.collection.selectedImage = base64;
-                          // $scope.addContact();    // Save contact
-                    });
-                  */
-                  }
-              }, function(error) {
-                  console.log('Error: ' + JSON.stringify(error));    // In case of error
-              });
-          };
-
-      });
-/*
-  //
-  // $scope.contact = {     // We will use it to save a contact
-  //
-  //         "displayName": "Gajotres",
-  //         "name": {
-  //             "givenName"  : "Dragannn",
-  //             "familyName" : "Gaiccc",
-  //             "formatted"  : "Dragannn Gaiccc"
-  //         },
-  //         "nickname": 'Gajotres',
-  //         "phoneNumbers": [
-  //             {
-  //                 "value": "+385959052082",
-  //                 "type": "mobile"
-  //             },
-  //             {
-  //                 "value": "+385914600731",
-  //                 "type": "phone"
-  //             }
-  //         ],
-  //         "emails": [
-  //             {
-  //                 "value": "dragan.gaic@gmail.com",
-  //                 "type": "home"
-  //             }
-  //         ],
-  //         "addresses": [
-  //             {
-  //                 "type": "home",
-  //                 "formatted": "Some Address",
-  //                 "streetAddress": "Some Address",
-  //                 "locality":"Zagreb",
-  //                 "region":"Zagreb",
-  //                 "postalCode":"10000",
-  //                 "country":"Croatia"
-  //             }
-  //         ],
-  //         "ims": null,
-  //         "organizations": [
-  //             {
-  //                 "type": "Company",
-  //                 "name": "Generali",
-  //                 "department": "IT",
-  //                 "title":"Senior Java Developer"
-  //             }
-  //         ],
-  //         "birthday": Date("08/01/1980"),
-  //         "note": "",
-  //         "photos": [
-  //             {
-  //                 "type": "base64",
-  //                 "value": $scope.collection.selectedImage
-  //
-  //             }
-  //         ],
-  //         "categories": null,
-  //         "urls": null
-  //     }
-  //
-  //     $scope.addContact = function() {
-  //         $cordovaContacts.save($scope.contact).then(function(result) {
-  //             console.log('Contact Saved!');
-  //         }, function(err) {
-  //             console.log('An error has occured while saving contact data!');
-  //         });
-  //     };
-*/
-
+  });
 });
 
 app.controller('AccountCtrl', function($scope) {
