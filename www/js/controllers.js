3049e24fc6b89c8c986369565cae80990fcb8949
XCACTUS\maciej.wojtasik <maciej.wojtasik@xcactus.pl>
2017-03-23 15:11:32 +0100

    permissions and loading images + refresh page works on android 6

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,109 +24,53 @@ app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 });
 
-app.controller('ImagesCtrl', function($scope, $cordovaImagePicker, $ionicPlatform) {
+app.controller('ImagesCtrl', function($scope, $state, $cordovaImagePicker, $ionicPlatform) {
   $scope.collection = {
     selectedImage : ''
   };
 
   $scope.allPermissions = [];
+  $scope.isReadPermission = false;
 
   $ionicPlatform.ready(function() {
 
-
-    this.checkPermissions = function(){
-        console.log("CHECK PERMISSIONS");
-        cordova.plugins.diagnostic.getPermissionsAuthorizationStatus(onCheckPermissions, onCheckPermissionsError, $scope.allPermissions);
-    }
-
-    this.init = function(){
-      for(var permission in cordova.plugins.diagnostic.runtimePermission){
-        console.log("PERMISSION " + permission + ", status: " + permission.status);
-        $scope.allPermissions.push(permission);
-      }
-      checkPermissions();
-    };
-
-    this.onCheckPermissions = function(statuses){
-        for(var permission in statuses){
-
-            var status = statuses[permission];
-            console.log("STATUS: " + status);
-        }
-    }
-
-    this.onCheckPermissionsError = function(error){
-        showAlert("Error checking permissions", "An error occurred while checking permissions: "+error);
-    }
-
-
-
-    init();
-
-
-    $scope.hasReadPermission = function() {
-      window.imagePicker.hasReadPermission(
-        function(result) {
-          // if this is 'false' you probably want to call 'requestReadPermission' now
-          alert(result);
-          requestReadPermission();
-        }
-      )
-    };
-
-
-    // var successHandler = angular.bind(this, this.userDetailsSuccessHandler);
-    // var errorHandler = angular.bind(this, this.userDetailsErrorHandler);
-    // this.$http.get(url).then(successHandler, errorHandler);
-    //
-    // EditUserProfilePopupController.prototype.userDetailsSuccessHandler = function(response) {
-    //   if (response) {
-    //     this.$scope.user = response.data.response;
-    //     this.$scope.userAvatarUrl = this.api.users + '/' + this.$scope.user.id + '/avatar-image';
-    //   } else {
-    //     this.alerts.showErrorAlert();
-    //   }
-    // };
-    //
-    // EditUserProfilePopupController.prototype.userDetailsErrorHandler = function(error) {
-    //   this.alerts.showErrorAlert();
-    // };
-
-
-    this.requestReadPermission = function () {
+    $scope.requestReadPermission = function () {
       console.log("PRZED WEJŚCIEM DO requestRuntimePermission");
 
-      var permissions = cordova.plugins.permissions;
-      console.log(permissions);
-
       cordova.plugins.diagnostic.requestRuntimePermission(function(status){
-        console.log("WEWNĄTRZ requestRuntimePermission");
-
         switch(status){
           case cordova.plugins.diagnostic.runtimePermissionStatus.GRANTED:
             console.log("Permission granted to use the camera");
+            $scope.isReadPermission = true;
+            $state.go($state.current, {}, {reload: true});
             break;
           case cordova.plugins.diagnostic.runtimePermissionStatus.NOT_REQUESTED:
             console.log("Permission to use the camera has not been requested yet");
+            $scope.isReadPermission = false;
+            $state.go($state.current, {}, {reload: true});
             break;
           case cordova.plugins.diagnostic.runtimePermissionStatus.DENIED:
             console.log("Permission denied to use the camera - ask again?");
+            $scope.isReadPermission = false;
+            $state.go($state.current, {}, {reload: true});
             break;
           case cordova.plugins.diagnostic.runtimePermissionStatus.DENIED_ALWAYS:
             console.log("Permission permanently denied to use the camera - guess we won't be using it then!");
+            $scope.isReadPermission = false;
+            $state.go($state.current, {}, {reload: true});
             break;
         }
       }, function(error){
           console.error("The following error occurred: "+error);
       }, cordova.plugins.diagnostic.permission.READ_EXTERNAL_STORAGE);
 
-
     };
 
+    $scope.init = function(){
+      $scope.requestReadPermission();
+    };
 
     $scope.getImage = function() {
-      requestReadPermission();
-      console.log("PRZED GET PICTURES");
 
       var options = {
           maximumImagesCount: 1,
@@ -137,15 +81,21 @@ app.controller('ImagesCtrl', function($scope, $cordovaImagePicker, $ionicPlatfor
 
       $cordovaImagePicker.getPictures(options)
         .then(function (results) {
-
           for (var i = 0; i < results.length; i++) {
             console.log('Image URI: ' + results[i]);
             $scope.collection.selectedImage = results[i];
           }
       }, function(error) {
-          console.log('Error: ' + JSON.stringify(error));    // In case of error
+          console.log('Error: ' + JSON.stringify(error));
       });
     };
+
+    $scope.fetchImage = function() {
+      //tu bedzie pytanko czy galeria czy foto
+      $scope.getImage();
+    };
+
+    $scope.init();
 
   });
 });
